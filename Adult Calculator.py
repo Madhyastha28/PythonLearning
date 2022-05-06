925699487ac6ee515e846c25ebe243287465265b
Subramanya <subbumadhya@gmail.com>
2022-05-06 07:42:46 +0530

    Added Elif

--- Adult Calculator.py
+++ Adult Calculator.py
@@ -2,5 +2,7 @@ User_Input = input("Enter the age")
 age = int(User_Input)
 if age > 18:
     print("He is an adult")
+elif age == 18:
+    print(" He is Just 18 Now!!")
 else:
     print("He is still a Minor")
