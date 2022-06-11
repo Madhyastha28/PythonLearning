8597e8ed77c243d6225cb804bd31bce1cbdda2e9
Subramanya <subbumadhya@gmail.com>
2022-06-11 18:12:15 +0530

    Selenium

---
+++ 04Multi.py
@@ -0,0 +1,5 @@
+# Whole Number Multiplication
+a = input("Please enter a value for a :  ")
+b = input("Please enter a value for b :  ")
+c = int(a) * int(b)
+print("The value after multiplication of", a, "with", b, "is", c)
