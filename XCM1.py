410a2b535835872e3e0cb6d98d25c762cd3d85d4
Subramanya <subbumadhya@gmail.com>
2022-06-15 07:39:54 +0530

    Selenium

--- XCM1.py
+++ XCM1.py
@@ -7,13 +7,24 @@ driver = Chrome(r"/Users/dell/Desktop/Training/chromedriver")
 sleep(3)
 driver.get("https://workflow.cchaxcess.com/xcmv2/account/PartialLogin?ReturnUrl=%2Fxcmv2%2F")
 sleep(1)
-driver.find_element(By.ID, "Email").send_keys("Subramanya@mail.com")
+driver.maximize_window()
+# Assert the login page
+a = driver.find_element(By.XPATH, "//h1[@class='text-primary']").text
+if a == ("CCH AxcessTM"):
+    print("Login page rendered")
+
+driver.find_element(By.ID, "Email").send_keys("Subramanya.madhyastha@gmail.com")
 driver.find_element(By.XPATH, "//button[normalize-space()='Submit']").click()
 # driver.close()
 sleep(2)
-driver.find_element(By.ID, "Password").send_keys("Password")
+driver.find_element(By.ID, "Password").send_keys("date****")
+sleep(1)
 driver.find_element(By.CLASS_NAME, "login-btn").click()
-sleep(2)
-
-driver.implicitly_wait(10)
+sleep(5)
+handles = driver.window_handles
+for items in handles:
+    print(items)
+    driver.switch_to.window(handles[0])
+    sleep(5)
+driver.find_element(By.XPATH,"//button[@id='skipNagScreen']").click()
 driver.close()
