31697be7ac11ebacdf68085ad7785fd181d70f35
Subramanya <subbumadhya@gmail.com>
2022-06-18 14:57:15 +0530

    Selenium

--- Sampl.py
+++ Sampl.py
@@ -1,3 +1,9 @@
 user_number = input("Enter a Number: ")
 print(int(user_number) * 2)
-# This is Sample and we are checking in the file
+a = int(user_number)
+a -= 2
+print(a)
+
+b = 5
+c = 5
+print(b == c)
