6f276b37dcd192007f5383ec928e02904f1ad97c
Subramanya <subbumadhya@gmail.com>
2022-07-17 18:53:01 +0530

    Selenium

---
+++ Lists.py
@@ -0,0 +1,11 @@
+M1 = int(input("Enter the marks of Student - 1 : "))
+M2 = int(input("Enter the marks of Student - 2 : "))
+M3 = int(input("Enter the marks of Student - 3 : "))
+M4 = int(input("Enter the marks of Student - 4 : "))
+M5 = int(input("Enter the marks of Student - 5 : "))
+M6 = int(input("Enter the marks of Student - 6 : "))
+Marks = [M1, M2, M3, M4, M5, M6]
+print(Marks)
+Marks.sort()
+print(Marks)
+
