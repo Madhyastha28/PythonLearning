c06840bb17f4582352d44b5e86d6e6ff621ac77d
Subramanya <subbumadhya@gmail.com>
2022-07-24 10:11:13 +0530

    Selenium

---
+++ Listaddition.py
@@ -0,0 +1,5 @@
+M1 = int(input("Enter Value for Number 1 : "))
+M2 = int(input("Enter Value for Number 2 : "))
+M3 = int(input("Enter Value for Number 3 : "))
+M4 = int(input("Enter Value for Number 4 : "))
+Val = [M1, M2, M3, M4]
