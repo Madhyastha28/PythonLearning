9f924fa7d28e597355b984df08e6a81b6a160c28
Subramanya <subbumadhya@gmail.com>
2022-07-24 18:06:05 +0530

    Selenium

--- Listaddition.py
+++ Listaddition.py
@@ -3,3 +3,9 @@ M2 = int(input("Enter Value for Number 2 : "))
 M3 = int(input("Enter Value for Number 3 : "))
 M4 = int(input("Enter Value for Number 4 : "))
 Val = [M1, M2, M3, M4]
+Sum = 0
+Sum += Val[0]
+Sum += Val[1]
+Sum += Val[2]
+Sum += Val[3]
+print("The sum of", Val[0], ",", Val[1], ",", Val[2], "and", Val[3], "is :", Sum)
