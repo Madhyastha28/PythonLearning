4a3a1ff89198583352823bd58ea5b7ce71155d4b
Subramanya <subbumadhya@gmail.com>
2022-08-08 19:51:33 +0530

    Selenium

---
+++ Dict.py
@@ -0,0 +1,13 @@
+Social = {
+    "Gift": "Something given to someone",
+    "Youtube": "Video Sharing Platform",
+    "Insta": "Picture Sharing Platform"
+}
+print("Print all the values")
+print(Social.items())
+print("Printing only keys")
+print(Social.keys())
+print("Printing only Values")
+print(Social.values())
+# Social.clear()
+# print(Social.items())
